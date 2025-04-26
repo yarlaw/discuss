43eb97bedce3f7c26a306b938e9382dc3733e7c2
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2025-04-26 16:05:18 +0000

    add file context

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,44 @@
 import streamlit as st
+from openai import OpenAI
 
-st.title("Demo")
+import random
+import time
+
+
+client = OpenAI(api_key = st.secrets["API_KEY"], base_url = st.secret["BASE_URL"])
+
+st.title("🗨️ LLM chat bot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "assistant", "content": "Let's start chatting! 👇"}]
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        assistant_response = client.chat.completions(model = st.secrets["MODEL"], messages = st.session.state.messages)
+
+        # Simulate stream of response with milliseconds delay
+        for chunk in assistant_response.split():
+            full_response += chunk + " "
+            time.sleep(0.05)
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+
